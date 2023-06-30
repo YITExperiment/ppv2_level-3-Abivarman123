f91c50a5f46fb3b2897dd8da634e2ae3ceb50482
Abivarman123 <132648966+Abivarman123@users.noreply.github.com>
2023-06-30 18:17:48 +0530

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,3 @@
+print("Hello User")
+user = input("what's your name? ")
+print("hello", user,"!")
---
+++ task1.py
@@ -0,0 +1,6 @@
+weight = "38 kg"
+print("weight is ", weight)
+
+height = "1.48m"
+print("height is ", height)
+
